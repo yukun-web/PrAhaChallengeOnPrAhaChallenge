bea79fccf5fbb377bef85193c4f36a94919c6ae7
yukun-web <yukun.web@gmail.com>
2025-10-07 13:09:04 +0900

    基底パッケージに関数コメントを追加

--- packages/fundamental/src/id.ts
+++ packages/fundamental/src/id.ts
@@ -1,5 +1,11 @@
 import { v4 } from "uuid";
 
+/**
+ * ランダムな UUID を生成します。
+ *
+ * @returns ランダムな UUID を返します。
+ * @remarks UUID を新しく生成するときは必ずこの関数を使用してください。
+ */
 export function uuid() {
   return v4();
 }
--- packages/fundamental/src/types.ts
+++ packages/fundamental/src/types.ts
@@ -1,8 +1,8 @@
 /**
  * 公称型を作成するヘルパー型です。
+ *
  * @template T 公称型にする型です。
  * @template Name 公称型の名前です。
- *
  * @example
  * type UserId = Nominal<string, 'UserId'>;
  */
